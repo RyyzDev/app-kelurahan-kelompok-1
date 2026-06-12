da1c17916ab47107d790855d90daf1c7174a3ff3
RyyzDev <fachriakbarkutubi@gmail.com>
2026-06-12 11:21:00 +0700

    static endpoint api

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: import.meta.env.REACT_APP_API_URL || 'http://localhost:5000/api',
+  baseURL: 'https://churchy-unpacifistic-velva.ngrok-free.dev/api' || 'http://localhost:5000/api',
   timeout: 10000,
 });
 
